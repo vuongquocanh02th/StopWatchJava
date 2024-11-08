995e6446d6895d08949032aa778ebc2e4ff9fb25
Vuong Quoc Anh <vuongquocanh300402@gmail.com>
2024-11-08 11:21:15 +0700

    stopwatch

---
+++ src/StopWatch.java
@@ -0,0 +1,15 @@
+public class StopWatch {
+    private long startTime;
+    private long endTime;
+    public StopWatch() {
+    }
+    public void start() {
+        startTime = System.currentTimeMillis();
+    }
+    public void stop() {
+        endTime = System.currentTimeMillis();
+    }
+    public long getElapsedTime() {
+        return endTime - startTime;
+    }
+}
